97188c81a5165da6415cd6b182a3d6117a8f106d
Manvir Singh Heer <40204204+manvirheer@users.noreply.github.com>
2020-10-05 00:08:56 -0700

    Update lab04-mhe-49.php

--- csv-creation/lab04-mhe-49.php
+++ csv-creation/lab04-mhe-49.php
@@ -1,8 +1,5 @@
 <?php
 
-//Student Number = 300304749
-//Name = Manvir Singh Heer
-
 //required files
 require_once("inc/config.inc.php");
 require_once("inc/file.inc.php");
@@ -10,38 +7,38 @@ require_once("inc/receipt.inc.php");
 require_once("inc/html.inc.php");
 require_once("inc/validation.inc.php");
 
-html_header_mhe_49();
+html_header();
 
 
 //If there are files to process
 if (!empty($_FILES))    {
     //Check for errors
-    $errors = validate_mhe_49();
+    $errors = validate();
     //No errors?
     if (empty($errors)) {
         //Read the file
-        $fileContents = read_mhe_49($_FILES['fileToUpload']['tmp_name']);
+        $fileContents = read($_FILES['fileToUpload']['tmp_name']);
         //Parse the contents
-        $items = parseItems_mhe_49($fileContents);
+        $items = parseItems($fileContents);
     }
 }
 
 //If the file is empty or error exit
 if (!empty($_FILES) && !empty($errors))  {
     //Show the errors
-    html_showErrors_mhe_49($errors);
+    html_showErrors($errors);
     //show the upload form
-    html_form_mhe_49();
+    html_form();
 } else if (isset($items))   {
-    html_table_mhe_49($items);
-    write_mhe_49($items);
+    html_table($items);
+    write_mhe($items);
 } else {
     //If you are here the user just requested the page.
-    html_form_mhe_49();
+    html_form();
 
 }
 
 
-//html_table_mhe_49();
+//html_table();
 
-html_footer_mhe_49();
+html_footer();
